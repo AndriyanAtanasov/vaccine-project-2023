b89bc6e587f310bfbb7a09f31872bbc047f0274f
AndriyanAtanasov <122821883+AndriyanAtanasov@users.noreply.github.com>
2023-11-27 23:35:32 +0200

    refactoring login page

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,16 +1,46 @@
+import { useContext } from "react";
+import useForm from "../../hooks/useForm.js";
+import AuthContext from "../../contexts/authContext.js";
+
+const LoginFormKeys = {
+  EMAIL: "email",
+  PASSWORD: "password",
+};
+
 const Login = () => {
+  const { loginSubmitHandler } = useContext(AuthContext);
+
+  const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+    [LoginFormKeys.EMAIL]: "",
+    [LoginFormKeys.PASSWORD]: "",
+  });
+
   return (
     <div className="coronata">
       <div className="content">
         <div className="container">
           <h1 className="mb-5">Login</h1>
           <div className="login-container">
-            <form className="login-form">
-              <label htmlFor="username">Username:</label>
+            <form className="login-form" onSubmit={onSubmit}>
+              <label htmlFor="email">Email:</label>
               <br />
-              <input type="text" id="username" /> <br />
+              <input
+                type="text"
+                id="email"
+                name={LoginFormKeys.EMAIL}
+                onChange={onChange}
+                value={values[LoginFormKeys.EMAIL]}
+              />
+              <br />
               <label htmlFor="password">Password:</label> <br />
-              <input type="password" id="password" /> <br />
+              <input
+                type="current-password"
+                id="password"
+                name={LoginFormKeys.PASSWORD}
+                onChange={onChange}
+                value={values[LoginFormKeys.PASSWORD]}
+              />
+              <br />
               <br />
               <button className="read_more" type="submit">
                 Login
