7d5822bb41b38f126280b49bc763b86529bf9aa9
AndriyanAtanasov <122821883+AndriyanAtanasov@users.noreply.github.com>
2023-11-27 23:35:03 +0200

    add useForm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,25 +10,35 @@ import Register from "./components/register/Register.jsx";
 import AddVaccine from "./components/vaccine-create/AddVaccine.jsx";
 import Login from "./components/login/Login.jsx";
 import VaccineInfo from "./components/vaccine-info/VaccineInfo.jsx";
+import { useState } from "react";
+import AuthContext from "./contexts/authContext.js";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/vaccinated" element={<VaccineList />}></Route>
-        <Route path="/news" element={<News />}></Route>
-        <Route path="/add-vaccine" element={<AddVaccine />}></Route>
-        <Route path="/vaccine/:vaccineId" element={<VaccineInfo />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+  const [auth, setAuth] = useState({});
 
-      <Footer />
-    </div>
+  const loginSubmitHandler = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loginSubmitHandler }}>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/vaccinated" element={<VaccineList />}></Route>
+          <Route path="/news" element={<News />}></Route>
+          <Route path="/add-vaccine" element={<AddVaccine />}></Route>
+          <Route path="/vaccine/:vaccineId" element={<VaccineInfo />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+
+        <Footer />
+      </div>
+    </AuthContext.Provider>
   );
 }
 
